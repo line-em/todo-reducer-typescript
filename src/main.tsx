a70e2e38a0e87362a7a8c3e5f7076bb69266d863
Aline Emily <1231866@isep.ipp.pt>
2024-07-24 18:40:25 +0100

    add libraries for styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
